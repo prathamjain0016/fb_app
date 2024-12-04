ad33892ecb5419c08719c1e1a3dc4f1e1d3b8f7d
prathamjain1080@gmail.com <prathamjain1080@gmail.com>
2024-12-04 10:57:12 +0530

    first commit

---
+++ Controllers/FacebookController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using FacebookApplication.Service;
+
+
+namespace FacebookApplication.Controllers
+{
+    public class FacebookController : Controller
+    {
+        private readonly FacebookService _facebookService;
+        public FacebookController(FacebookService facebookService)
+        {
+            _facebookService = facebookService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var posts = await _facebookService.GetFacebookPostsAsync();
+            ViewData["Posts"] = posts;
+            return View();
+        }
+
+    }
+}
---
+++ Service/FacebookService.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+
+namespace FacebookApplication.Service
+{
+    public class FacebookService
+    {
+       
+        
+            private static readonly string PageId = "your-page-id";
+            private static readonly string AccessToken = "your-access-token";
+
+            public async Task<string> GetFacebookPostsAsync()
+            {
+                string url = $"https://graph.facebook.com/{PageId}/posts?access_token={AccessToken}";
+                using HttpClient client = new HttpClient();
+                var response = await client.GetStringAsync(url);
+                var posts = JObject.Parse(response);
+                return posts.ToString();
+            }
+        
+    }
+}
